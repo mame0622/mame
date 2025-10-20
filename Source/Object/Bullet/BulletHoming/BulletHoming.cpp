1acd42840ecb156cdaccecfcc634343cb2a459dd
YoudaRikuto <2220364@ecc.ac.jp>
2025-10-20 19:24:37 +0900

    回転の速度できない

--- Source/Object/Bullet/BulletHoming/BulletHoming.cpp
+++ Source/Object/Bullet/BulletHoming/BulletHoming.cpp
@@ -70,12 +70,31 @@ void BulletHoming::Launch(const DirectX::XMFLOAT2& moveDirection)
 
 void  BulletHoming::Pursuit(const float& elapsedTime)
 {
+    // 前方向ベクトル算出
+    const float angleRadians = DirectX::XMConvertToRadians(GetTransform()->GetAngle());
+    const DirectX::XMFLOAT2 forward = { sinf(angleRadians),-cosf(angleRadians) };
+
+    // 進行方向ベクトル算出
     const DirectX::XMFLOAT2 playerCenterPosition = PlayerManager::Instance().GetTransform()->GetCenterPosition();
     const DirectX::XMFLOAT2 homingCenterPosition = GetTransform()->GetCenterPosition();
     const DirectX::XMFLOAT2 moveVec = playerCenterPosition - homingCenterPosition;
     length_ = XMFloat2Length(moveVec);
     moveDirection_ = XMFloat2Normalize(moveVec);
-    GetTransform()->AddPosition(moveDirection_ * moveSpeed_ * elapsedTime);
+    const float dot = XMFloat2Dot(moveDirection_, forward);
+    DirectX::XMFLOAT2 cent = moveDirection_ - (forward * dot);
+    const float length = XMFloat2Length(cent);
+    if (length > 1.0f)
+    {
+        cent /= length;
+    }
+    const DirectX::XMFLOAT2 Vec = cent * 5.0f;
+
+    //const float angle = acosf(dot);
+    //float radians = angle;
+    //const DirectX::XMFLOAT2 Vec = { sinf(angle) + forward.x ,forward.y - cosf(angle)};
+
+    GetTransform()->AddPosition(Vec * moveSpeed_ * elapsedTime);
+
     // 角度
     GetTransform()->SetAngle(DirectX::XMConvertToDegrees(atan2f(moveDirection_.y, moveDirection_.x) + DirectX::XM_PIDIV2));
 }
